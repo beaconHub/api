e5df1686ee8390dcb293a38e3dc63fa1cfe503ca
Harry Ng <harryworld@gmail.com>
2014-08-16 14:18:24 +0800

    Set heroku

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   resources :beacons
 
+  root 'beacons#index'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
